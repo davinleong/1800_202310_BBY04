ddc36da449e70eba0cb4b162d1c8b4af4438a295
Ran Park <kpark90@my.bcit.ca>
2023-03-03 01:27:31 -0800

    added authentication feature

---
+++ public/js/firebaseAPI_BBY04.js
@@ -0,0 +1,19 @@
+//---------------------------------------
+//---------------------------------------
+// Your web app's Firebase configuration
+//---------------------------------------
+var firebaseConfig = {
+  apiKey: "AIzaSyAfVSBReObbl0g6_hHhfqbVMfZQT829vgw",
+  authDomain: "bby04-ed8d8.firebaseapp.com",
+  projectId: "bby04-ed8d8",
+  storageBucket: "bby04-ed8d8.appspot.com",
+  messagingSenderId: "759882351704",
+  appId: "1:759882351704:web:988fa4dc13e7d02212b62d",
+  measurementId: "G-BLCG5V0Z3T",
+};
+//-------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//-------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
