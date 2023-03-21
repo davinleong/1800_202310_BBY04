2be0f00aaadfa5874b733b48f3c16ee89d740b8a
Hoa <tnguyen568@my.bcit.ca>
2023-03-21 11:30:13 -0700

    Deleted some not using codes, created skeleton for newsFeed

--- index.js
+++ index.js
@@ -103,6 +103,13 @@ app.get("/favourite", function (req, res) {
   res.send(doc);
 });
 
+app.get("/newsFeed", function (req, res) {
+  //console.log(process.env);
+  // retrieve and send an HTML document from the file system
+  let doc = fs.readFileSync("./app/html/newsFeed.html", "utf8");
+  res.send(doc);
+});
+
 // for resource not found (i.e., 404)
 app.use(function (req, res, next) {
   // this could be a separate file too - but you'd have to make sure that you have the path
