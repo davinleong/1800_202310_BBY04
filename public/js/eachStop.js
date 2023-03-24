79271f7fda22efa4da629bade4f4b16964600909
Ran Park <kpark90@my.bcit.ca>
2023-03-24 07:46:41 -0700

    Some changes in eachStop.html/js

--- public/js/eachStop.js
+++ public/js/eachStop.js
@@ -16,9 +16,22 @@ function displayBusStopInformation() {
     document.getElementById("busStopName").innerHTML = busStopsName;
     let imgEvent = document.querySelector(".bus-img");
     imgEvent.src = "/images/" + busStopsCode + ".jpg";
-  }
+    //newcard.querySelector('i').id = 'save-' + docID;
+    document.getElementById("bookmark").querySelector('i').id = 'save-' + docID;
+    // this line will call a function to save the hikes to the user's document             
+    //newcard.querySelector('i').onclick = () => toggleBookmark(docID);
+    document.getElementById("bookmark").querySelector('i').onclick = () => toggleBookmark(docID);
+    //Ensure that the bookmark displays correctly as filled
+    //if it is already in the favourites
+    currentUser.get().then(userDoc => {
+        //get the user name
+        var bookmarks = userDoc.data().bookmarks;
+        if (bookmarks.includes(docID)) {
+           document.getElementById('save-' + docID).innerText = 'bookmark';
+        }
+      })   
 
-  )
+  })
 
 }
 displayBusStopInformation();
@@ -30,6 +43,42 @@ function saveBusDocumentIDAndRedirect() {
   window.location.href = 'feed.html';
 }
 
+//Reviews
+function populateReviews() {
+  let hikeCardTemplate = document.getElementById("reviewCardTemplate");
+  let hikeCardGroup = document.getElementById("reviewCardGroup");
+
+  let params = new URL(window.location.href); //get the url from the searbar
+  let busID = params.searchParams.get("docID");
+
+  // doublecheck: is your collection called "Reviews" or "reviews"?
+  db.collection("reviews")
+    .where("busDocID", "==", busID)
+    .get()
+    .then((allReviews) => {
+      reviews = allReviews.docs;
+      console.log(reviews);
+      reviews.forEach((doc) => {
+        var title = doc.data().name;       // get value of the "name" key
+        var details = doc.data().details;  // get value of the "details" key
+				var busCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+        var busSchedule = doc.data().schedule; //gets the bus schedule
+        var docID = doc.id;
+
+        let reviewCard = reviewCardTemplate.content.cloneNode(true);
+        newcard.querySelector('.card-title').innerHTML = title;
+        newcard.querySelector('.card-schedule').innerHTML = busSchedule;
+        newcard.querySelector('.card-image').src = `./images/${busCode}.jpg`; //Example: BUS01.jpg
+        newcard.querySelector('.card-text').innerHTML = details;
+        newcard.querySelector('a').href = "eachBus.html?docID="+docID;
+        console.log("this card");
+        reviewCardGroup.appendChild(reviewCard);
+      });
+    });
+}
+populateReviews();
+
+//Bookmark 
 function toggleBookmark() {
   var currentUser = firebase.auth().currentUser; 
   currentUser.get().then(userDoc => {
